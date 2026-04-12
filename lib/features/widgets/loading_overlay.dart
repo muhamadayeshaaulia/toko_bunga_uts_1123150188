a5f2495f4be377d9bcfbcf3b7d6c961125f9add0
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 11:48:52 +0700

    FASE 3 : loading_overlay.dart
    menambahkan final widget, isloading $ message

--- lib/features/widgets/loading_overlay.dart
+++ lib/features/widgets/loading_overlay.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class LoadingOverlay extends StatelessWidget {
+  final Widget child;
+  final bool isLoading;
+  final String? message;
+
   const LoadingOverlay({super.key});
 
   @override
