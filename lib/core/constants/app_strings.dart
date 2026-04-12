55b2778cbd947c27e4310ee9a09a02b086aff7ab
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 08:33:56 +0700

    header : add string auth
    menambahkan beberapa keperluan untuk daftar masuk keluar email pw namalengkap dan lainnya

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -3,4 +3,16 @@ import 'package:flutter/material.dart';
 class AppStrings {
   //app
   static const String appName = 'Flower Shop';
+
+  //Authentication
+  static const String register = 'Daftar';
+  static const String login = 'Masuk';
+  static const String logout = 'Keluar';
+  static const String email = 'Email';
+  static const String password = 'Password';
+  static const String confirmPassword = 'Konfirmasi Password';
+  static const String fullName = 'Nama Lengkap';
+  static const String forgotPassword = 'Lupa Password?';
+  static const String resetPassword = 'Reset Password';
+
 }
