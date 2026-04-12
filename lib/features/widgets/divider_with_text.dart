d402845ac33f6696aba529f2b75fd7d39fb7df14
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 11:06:38 +0700

    FASE 3 : divider_with_text.dart
    membuat row childern dengan pading horizontal

--- lib/features/widgets/divider_with_text.dart
+++ lib/features/widgets/divider_with_text.dart
@@ -6,6 +6,16 @@ class DividerWithText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        const Expanded(child: Divider()),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text(text, style: TextStyle(color: Colors.grey.shade500, fontSize: 13)),
+        ),
+        const Expanded(child: Divider()),
+      ],
+    );
+
   }
 }
