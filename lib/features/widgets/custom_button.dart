3ae670251a134ca65cc9456da081c844eb2a96a1
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 10:50:48 +0700

    FASE 3 : add final in custombutton
    memberikan final string,voidcallback,bool widget

--- lib/features/widgets/custom_button.dart
+++ lib/features/widgets/custom_button.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 enum ButtonType { primary, outline, text }
 
 class CustomButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+  final bool isLoading;
+  final ButtonVariant variant;
+  final Widget? icon;
+
   const CustomButton({super.key});
 
   @override
