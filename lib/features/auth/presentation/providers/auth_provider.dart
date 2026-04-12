3ff5ac1686b7216fdcb61113f26f77df0dab7f47
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 14:57:49 +0700

    FASE 4 : auth_provider.dart
    membuat future untuk login menggunakan email

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -48,3 +48,33 @@ class AuthProvider extends ChangeNotifier {
   notifyListeners();
   return true;
 }
+
+Future<bool> loginWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    _setLoading();
+    try {
+      final credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      _firebaseUser = credential.user;
+
+
+      // Cek apakah email sudah diverifikasi
+      if (!(_firebaseUser?.emailVerified ?? false)) {
+        _status = AuthStatus.emailNotVerified;
+        notifyListeners();
+        return false;
+      }
+
+
+      // Email terverifikasi → dapatkan token Firebase → kirim ke backend
+      return await _verifyTokenToBackend();
+    } on FirebaseAuthException catch (e) {
+      _setError(_mapFirebaseError(e.code));
+      return false;
+    }
+  }
+
