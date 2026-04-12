20a403bbdb6cd27a0a1ea48e839ea160ac2adb8c
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 10:36:30 +0700

    FASE 3 : add final in customtestfield
    menambahkan final stink widget bool dan lainnya utnuk keperluan customtextfield

--- lib/features/widgets/custom_text_field.dart
+++ lib/features/widgets/custom_text_field.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
+  final String label;
+  final String hint;
+  final TextEditingController controller;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final bool enabled;
+
   const CustomTextField({super.key});
 
   @override
