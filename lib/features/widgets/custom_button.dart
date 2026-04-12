2f378c033615068c8a5ae17381535f1af491b87a
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 10:45:38 +0700

    FASE 3 : add custoom_button.dart
    membuat class cutombutton dan memberikan 3 varian primary,out,text

---
+++ lib/features/widgets/custom_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+enum ButtonType { primary, outline, text }
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
