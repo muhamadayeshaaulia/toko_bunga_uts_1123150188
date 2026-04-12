a7ac4020adc7f08dc17e3310b54622b0af15f896
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 11:11:50 +0700

    FASE 3 : google_sign_in_button.dart
    membuat final coidcallback dan bool di dalam class

--- lib/features/widgets/google_sign_in_button.dart
+++ lib/features/widgets/google_sign_in_button.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class GoogleSignInButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final bool isLoading;
+
   const GoogleSignInButton({super.key});
 
   @override
