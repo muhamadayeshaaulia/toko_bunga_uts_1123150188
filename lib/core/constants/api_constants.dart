16620d2b360c8d5f8f24bce76a7b9c2438cde2a7
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 08:46:04 +0700

    header : add url in api_constants.dart
    membuat beberpa url keperluan proyek seperti auth verytoken produk dan untuk halaman utama

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ApiConstants {
-  
+  static const String baseUrl = 'http://192.168.0.28:8080/v1';
+ 
+  // Auth endpoints
+  static const String verifyToken = '/auth/verify-token';
+ 
+  // Product endpoints
+  static const String products = '/products';
+ 
+  // Timeout
+  static const int connectTimeout = 15000;
+  static const int receiveTimeout = 15000;
+
 }
