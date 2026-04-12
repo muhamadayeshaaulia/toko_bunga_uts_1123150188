399710c8a9be01cf544d690c41e342d2a2ae3e34
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 10:38:12 +0700

    FASE 3 : add required in customtextfield
    menambahkan beberapa required hint dan controller dan keperluan seperti suffix prefix icon dan lainnya

--- lib/features/widgets/custom_text_field.dart
+++ lib/features/widgets/custom_text_field.dart
@@ -11,7 +11,17 @@ class CustomTextField extends StatelessWidget {
   final Widget? prefixIcon;
   final bool enabled;
 
-  const CustomTextField({super.key});
+  const CustomTextField({super.key,
+  required this.label,
+    required this.hint,
+    required this.controller,
+    this.obscureText = false,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.enabled = true,
+});
 
   @override
   Widget build(BuildContext context) {
