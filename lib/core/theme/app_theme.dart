9291f73eba56b4d26323ac99acc5eb7195c7fc58
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 09:06:07 +0700

    FASE 2 : scaafold
    menambahkan scafold backgoround color

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,6 +4,21 @@ class AppTheme {
   static ThemeData get light {
     return ThemeData(
       useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.primary,
+        primary: AppColors.primary,
+        surface: AppColors.surface,
+        background: AppColors.background,
+        error: AppColors.error,
+      ),
+    scaffoldBackgroundColor: AppColors.background,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.primary,
+        foregroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: false,
+      ),
+
     );
   }
 }
