4937df0d983d0b4202c103d12f9609d6d6f88ff4
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 14:55:29 +0700

    FASE 4 : auth_provider.dart
    membuat class auth provider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
  enum AuthStatus{
   initial,
   loading,
@@ -6,4 +9,20 @@ import 'package:flutter/material.dart';
   unauthenticated,
   emailNoVerified,
   error,
+ }
+class AuthProvider extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  AuthStatus _status = AuthStatus.initial;
+  User?     _firebaseUser;
+  String?   _backendToken; 
+  String?   _errorMessage;
+
+  AuthStatus get status       => _status;
+  User?      get firebaseUser  => _firebaseUser;
+  String?    get backendToken  => _backendToken;
+  String?    get errorMessage  => _errorMessage;
+  bool       get isLoading     => _status == AuthStatus.loading; 
+
  }
