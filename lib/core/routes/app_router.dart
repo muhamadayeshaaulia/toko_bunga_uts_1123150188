50b678a2400bc6a90139c3ef84f8ba33a0e1b111
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 09:57:36 +0700

    FASE 2 : add map router
    membuat map untuk mengarahkan link ke link widget

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppRouter {
-  
+  static const String splash      = '/';
+  static const String login       = '/login';
+  static const String register    = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String dashboard   = '/dashboard';
+
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash:      (_) => const SplashPage(),
+    login:       (_) => const LoginPage(),
+    register:    (_) => const RegisterPage(),
+    verifyEmail: (_) => const VerifyEmailPage(),
+    dashboard:   (_) => const AuthGuard(child: DashboardPage()),
+  };
+
 }
