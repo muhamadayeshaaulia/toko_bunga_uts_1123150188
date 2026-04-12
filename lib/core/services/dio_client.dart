7443eb2c11b0b6d184940bae5d149022a8bb78ca
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 09:38:23 +0700

    FASE 2 : dio_client.dart
    membuat static dio get instantce

--- lib/core/services/dio_client.dart
+++ lib/core/services/dio_client.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 class DioClient {
-  
+  static Dio? _instance;
+  static Dio get instance{
+    _instance ??= _createDio();
+    return _instance!;
+  }
 }
