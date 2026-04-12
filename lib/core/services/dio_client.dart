fe5393953a687f0a914219bc8b3d15cd71515475
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 09:41:50 +0700

    FASE 2 : dio_client.dart
    mengambil apiconstant dengan type json

--- lib/core/services/dio_client.dart
+++ lib/core/services/dio_client.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import '../constants/api_constants.dart';
 
 class DioClient {
   static Dio? _instance;
   static Dio get instance{
-    _instance ??= _createDio();
+    _instance ??= _createDio(); // Singleton pattern
     return _instance!;
+  }
+  static Dio _createDio(){
+    final dio = Dio(BaseOptions(
+      baseUrl: ApiConstants.baseUrl,
+      connectTimeout: Duration(milliseconds: ApiConstants.connectTimeout),
+      receiveTimeout: Duration(milliseconds: ApiConstants.receiveTimeout),
+      headers: {'Content-Type': 'application/json'},
+    ));
+
+    
   }
 }
