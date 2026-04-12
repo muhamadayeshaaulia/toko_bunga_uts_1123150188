2a42b627c886b2d58555b7bf6c9184c7205c32d0
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-12 11:03:17 +0700

    FASE 3 : divider_with_text,dart
    membuat class dividerwithtext

---
+++ lib/features/widgets/divider_with_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DividerWithText extends StatelessWidget {
+  const DividerWithText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
