fc3478af9fea21a3d085fa97a417bcf4eb2b2791
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-17 17:34:50 +0700

    FASE 7 : update main
    menghubungkan app router ke sini

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'core/routes/app_router.dart';
 import 'features/auth/presentation/providers/auth_provider.dart';
-import 'features/dashboard/presentation/providers/product_provider.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
- 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => ProductProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'My App',
+        initialRoute: AppRouter.splash,
+        routes: AppRouter.routes,
+      ),
+    );
+  }
+}
