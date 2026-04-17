4373d7a577542100c7a93e833721dc03ff936935
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-17 17:16:51 +0700

    FASE 7 : memperbaiki routes
    menambahkan class authguard

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uts_1123150188_semester6/features/auth/presentation/pages/Register_page.dart';
 import 'package:uts_1123150188_semester6/features/auth/presentation/pages/login_page.dart';
 import 'package:uts_1123150188_semester6/features/auth/presentation/pages/verify_email_page.dart';
 import 'package:uts_1123150188_semester6/features/dashboard/presentation/pages/dashboard_page.dart';
+import '../../features/auth/presentation/providers/auth_provider.dart';
+import 'router_guard.dart';
 
 
+class AuthGuard extends StatelessWidget {
+  final Widget child;
+
+  const AuthGuard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final status = context.watch<AuthProvider>().status;
+ 
+    return switch (status) {
+      AuthStatus.authenticated    => child,                   // Lanjut
+      AuthStatus.emailNotVerified => const VerifyEmailPage(), // Redirect
+      _                           => const LoginPage(),       // Redirect login
+    };
+  }
+}
 
 class AppRouter {
   static const String splash      = '/';
@@ -20,8 +39,9 @@ class AppRouter {
     register:    (_) => const RegisterPage(),
     verifyEmail: (_) => const VerifyEmailPage(),
     dashboard:   (_) => const AuthGuard(child: DashboardPage()),
+
   };
 
-  
+    
 
 }
