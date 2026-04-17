af474eb88f694ef23c95d7d814058a7a85925ece
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-04-17 15:56:13 +0700

    FASE 5 : product_model.dart
    membuat class product model

---
+++ lib/features/dashboard/data/model/product_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ProductModel {
+  final int    id;
+  final String name;
+  final double price;
+  final String imageUrl;
+  final String category;
+
+
+  const ProductModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.imageUrl,
+    required this.category,
+  });
+
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+    id:       json['id']        as int,
+    name:     json['name']      as String,
+    price:    (json['price'] as num).toDouble(),
+    imageUrl: json['image_url'] as String,
+    category: json['category'] as String,
+  );
+
+
+  @override
+  List<Object?> get props => [id, name, price];
+}
